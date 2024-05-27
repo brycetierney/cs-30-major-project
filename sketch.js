f8faacebf0a0b3b33ae1510683e75009d4212caf
brycetierney <161519956+brycetierney@users.noreply.github.com>
2024-05-27 14:11:33 -0600

    setting up the preview icons that will display
    added a lot of much needed work and thought out how i will be adding the preview's for the games on the home screen

--- sketch.js
+++ sketch.js
@@ -1,33 +1,47 @@
-// Steak
+// Steak Casino
 // Bryce Tierney
 // June something, 2024
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-// Setting variables. gameState starts as the homne screen and
+// Setting variables. gameState starts as the homne screen and ETCCCCC
 let gameState = "home";
-let betAmount;
-let targetMultiplier = 2.00;
-// how to get sound, need to get a click mp3 sound and use preload
 
-// Loading a sound file, images for the preview screens, 
+// Limbo preview x and y
+let limboX = width/4;
+let limboY = height/4;
+
+// // Plinko preview x and y
+// let plinkoX = width/4;
+// let plinkoY = height/4;
+// // Mines preview x and y
+// let minesX = width/4;
+// let minesY = height/4;
+
+
+// how to get sound, need to get a click mp3 sound and use preload ETCCCCC
+
+// Loading a sound file, images for the preview screens, ETCCCCC
 function preload() {
-  sound = loadsound("WHATEVER SOUND I need to load");
+  sound = loadSound("WHATEVER SOUND I need to load"); // Add what you need here ETCCCCC
+  limbo = loadImage("");
+
+  // plinko = loadImage("");
+  // mines = loadImage("");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
-// Setting gameState based off which game you chose to play, starting originally as homeScreen
+// Displaying whichever game you clicked, at the start it is homeScreen
 function draw() {
   background(220);
-
   if (gameState === "home") {
     drawHomeScreen();
   }
-  if (gameState === "limbo") {
+  if (gameState === "limbo") { 
     darwLimboGame();
   }
   if (gameState === "plinko") {
@@ -40,15 +54,30 @@ function draw() {
 
 
 function drawHomeScreen() {
-  // Eventually make it show images in preload function instead of just having coloured squares
-  fill(0);
-  square(width/4, height/8, width/4)
+  // Make it show images by creating them in the preload function, need to download or something???
+
+  // Displaying limbo preview
+  limbo(limboX, limboY, limboSize);
+
+  // Displaying plinko preview
+  plinko(plinkoX, plinkoY, plinkoSize);
+
+  // Displaying mines preview
+  mines(minesX, minesY, minesSize)
 
   // Use the button thing instead of this probably
+  if (mousePressed && mouseX > limboX - width/10 && mouseX < limboX + width/10 && mouseY > limboY - height/10 && mouseY < limboY + height/10) {
+    gameState = "limbo";
+  }
 
 }
 
 
-function mousePressed() {
-  if (mousePressed && mosue 
+function darwLimboGame() {
+  // Setting variables for specifically the limbo game
+  let betAmount;
+  let targetMultiplier;
+  // let limboX = width/4;
+  // let limboY = height/4;
+
 }
